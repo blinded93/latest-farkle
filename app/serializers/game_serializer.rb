78daded2a2721c50fda34e40e541feb666767ea2
Kyle Hughes <blinded93@gmail.com>
2019-11-23 18:32:16 -0800

    update serializers

--- app/serializers/game_serializer.rb
+++ app/serializers/game_serializer.rb
@@ -1,4 +1,20 @@
 class GameSerializer < ActiveModel::Serializer
-  attributes :id
-  has_one :user
+  attributes :id, :inProgress, :winner, :lastTurn, :currentPlayer, :lastPlayer
+  has_many :scorecards
+
+  def inProgress
+    object.in_progress
+  end
+
+  def lastTurn
+    object.last_turn
+  end
+
+  def currentPlayer
+    object.current_player
+  end
+
+  def lastPlayer
+    object.last_player
+  end
 end
--- app/serializers/scorecard_serializer.rb
+++ app/serializers/scorecard_serializer.rb
@@ -1,5 +1,7 @@
 class ScorecardSerializer < ActiveModel::Serializer
-  attributes :id, :score, :scores_per_turn, :name, :color
-  has_one :user
-  has_one :game
+  attributes :id, :name, :color, :score, :scoresPerTurn
+
+  def scoresPerTurn
+    object.scores_per_turn
+  end
 end
