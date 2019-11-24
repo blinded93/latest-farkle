e55575d404fd497c4f836c8436cf2350e6e44b66
Kyle Hughes <blinded93@gmail.com>
2019-11-23 21:53:22 -0800

    puts rails secret base

--- app/lib/json_web_token.rb
+++ app/lib/json_web_token.rb
@@ -2,7 +2,8 @@ class JsonWebToken
   def self.encode(payload)
     exp = { exp: Time.now.to_i + 86400 }
     exp_payload = payload.merge(exp)
-    puts JWT.encode(exp_payload, Rails.application.secrets.secret_key_base)
+    puts Rails.application.sercrets.secret_key_base
+    JWT.encode(exp_payload, Rails.application.secrets.secret_key_base)
   end
 
   def self.decode(token)
