bccbe509b59a20c11ab4cf75af30dce5f0d023fe
Kyle Hughes <blinded93@gmail.com>
2019-11-23 21:00:29 -0800

    pre-compile react app

---
+++ public/precache-manifest.a212d0b497560bf83f8351ef0e154672.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3964033e9685349333d5a83eed94a28",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6c28d4c0d10d90daff1d",
+    "url": "/static/css/main.122e41ff.chunk.css"
+  },
+  {
+    "revision": "cbc7c9c3fcdab030bda7",
+    "url": "/static/js/2.f6808acd.chunk.js"
+  },
+  {
+    "revision": "6c28d4c0d10d90daff1d",
+    "url": "/static/js/main.05efb3b8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
