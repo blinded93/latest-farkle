7302af8a42d2d30aeafb97cedc248c9727abaf3b
Kyle Hughes <blinded93@gmail.com>
2019-11-24 11:56:01 -0800

    change secrets access to rails master key

--- app/lib/json_web_token.rb
+++ app/lib/json_web_token.rb
@@ -3,12 +3,12 @@ class JsonWebToken
     exp = { exp: Time.now.to_i + 86400 }
     exp_payload = payload.merge(exp)
     puts Rails.application.secrets.secret_key_base
-    JWT.encode(exp_payload, Rails.application.secrets.secret_key_base)
+    JWT.encode(exp_payload, ENV['RAILS_MASTER_KEY'])
   end
 
   def self.decode(token)
     return HashWithIndifferentAccess.new(
-      JWT.decode(token, Rails.application.secrets.secret_key_base)[0]
+      JWT.decode(token, ENV['RAILS_MASTER_KEY'])
     )
   rescue
     nil
