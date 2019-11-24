c0a6ee43ab36829fedaede76cea20e2047b91ffc
Kyle Hughes <blinded93@gmail.com>
2019-11-23 21:51:05 -0800

    change puts location

--- app/lib/json_web_token.rb
+++ app/lib/json_web_token.rb
@@ -1,11 +1,8 @@
 class JsonWebToken
   def self.encode(payload)
-    puts payload
     exp = { exp: Time.now.to_i + 86400 }
-    puts exp
     exp_payload = payload.merge(exp)
-    puts exp_payload
-    JWT.encode(exp_payload, Rails.application.secrets.secret_key_base)
+    puts JWT.encode(exp_payload, Rails.application.secrets.secret_key_base)
   end
 
   def self.decode(token)
