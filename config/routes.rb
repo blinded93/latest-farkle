0610d76cc6ab9094af0730fa64b1c8415c455030
Kyle Hughes <blinded93@gmail.com>
2019-11-23 17:55:53 -0800

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+	resources :users
+  resources :scorecards
+  resources :games
+  post '/auth_user' => 'authentication#authenticate_user'
+  post '/verify_user' => 'authentication#verify_user'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
